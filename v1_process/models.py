d4739200d7681eb0ac663593ef4773f6ce9333d9
Dani Romero <brayan.romero@evolutioncons.com>
2023-09-07 07:01:46 -0500

    RETFUE-001: models created

--- v1_process/models.py
+++ v1_process/models.py
@@ -4,7 +4,37 @@ from django.db import models
 
 class WithholdingTax(models.Model):
     is_foreigner = models.BooleanField(blank=False)
-    name = models.CharField('name', max_length=128)
+    percentage_foreigner = models.DecimalField(max_digits=10, decimal_places=2)
+    gross_total_incomes = models.IntegerField()
+    gross_voluntary_withholding = models.IntegerField()
+    social_security_values = models.JSONField()
+    food_bonus = models.IntegerField()
+    dependent_sum = models.IntegerField()
+    procedure_type = models.CharField(max_length=1)
+    argument_procedure_type = models.DecimalField(max_digits=10, decimal_places=2)
+    payroll_date = models.DateField()
+    initial_date_contract = models.DateField()
+    final_date_contract = models.DateField()
+    housing_relief = models.IntegerField()
+    health_relief = models.IntegerField()
+    housing_relief_cap =  models.IntegerField()
+    health_relief_cap = models.IntegerField()
+    uvt_value = models.IntegerField()
+    non_constitutive_incomes_cap = models.IntegerField()
+    minimum_month_salary = models.IntegerField()
+    exempt_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    exempt_cap = models.IntegerField()
+    accumulated_exemption = models.IntegerField()
+    exempts_periodicity = models.CharField(max_length=1)
+    deductible_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    deductible_cap = models.IntegerField()
+    accumulated_deductible = models.IntegerField()
+    voluntary_cap_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    voluntary_cap = models.IntegerField()
+    dependent = models.BooleanField(blank=False)
+    dependent_cap = models.IntegerField()
+    dependent_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 
 class SocialSecurity(models.Model):
--- v1_process/serializers.py
+++ v1_process/serializers.py
@@ -5,7 +5,37 @@ from v1_process.models import SocialSecurity, WithholdingTax
 class WithholdingTaxSerializer(serializers.ModelSerializer):
     class Meta:
         model = WithholdingTax
-        fields = ['is_foreigner', 'name']
+        fields = ['is_foreigner',
+                'percentage_foreigner', 
+                'gross_total_incomes',
+                'gross_voluntary_withholding', 
+                'social_security_values', 
+                'food_bonus',
+                'dependent_sum', 
+                'procedure_type', 
+                'argument_procedure_type',
+                'payroll_date', 
+                'initial_date_contract', 
+                'final_date_contract',
+                'housing_relief', 
+                'health_relief', 
+                'housing_relief_cap',
+                'health_relief_cap', 
+                'uvt_value', 
+                'non_constitutive_incomes_cap',
+                'minimum_month_salary', 
+                'exempt_percentage', 
+                'exempt_cap',
+                'accumulated_exemption', 
+                'exempts_periodicity',
+                'deductible_percentage', 
+                'deductible_cap', 
+                'accumulated_deductible',
+                'voluntary_cap_percentage', 
+                'voluntary_cap', 
+                'dependent',
+                'dependent_cap', 
+                'dependent_percentage']
 
 
 class SocialSecuritySerializer(serializers.ModelSerializer):
