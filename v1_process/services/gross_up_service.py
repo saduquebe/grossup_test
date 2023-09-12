05688e67a8913aec4fc643a730e9042f876e315a
dangel <bromeroa@unal.edu.co>
2023-09-12 12:15:22 -0500

    feat: pass model data to gross up service

--- v1_process/services/gross_up_service.py
+++ v1_process/services/gross_up_service.py
@@ -3,20 +3,21 @@ Fill this service with
 """
 from v1_process.services.withholding_tax_service import WithholdingTaxService
 from v1_process.services.social_security_service import SocialSecurityService
-
+from v1_process.models import SocialSecurity, Employee, WithholdingTax
 
 class GrossUpService:
 
-    def __init__(self, social_security_service: SocialSecurityService, withholding_tax_service: WithholdingTaxService):
-        self.social_security_service = social_security_service
-        self.withholding_tax_service = withholding_tax_service
-
+    def __init__(self, employees: list):
+        self.employees = employees
     def _internal_method(self, attr):
         return attr
 
+    def _gross_up(self, social_security:SocialSecurity,withholding_tax:WithholdingTax):
+        pass
     def exec(self) -> str:
-        return "Llamado exitoso" , {self.social_security_service.employee_id}
+        for employee in self.employees:
+            self._gross_up(employee.social_security, employee.withholding_tax)
     
-    def _save_to_database():
+    def _save_to_database(self):
         #Logic to connect to Model class and persist data
         return
--- v1_process/views.py
+++ v1_process/views.py
@@ -25,35 +25,11 @@ def exec_excel_pipeline(request):
         reader_company_data = csv.DictReader(decoded_company_data)
         reader_employee_data = csv.DictReader(decoded_employee_data)
 
-        load_company_employees_data(reader_company_data, reader_employee_data)
+        employees = load_company_employees_data(reader_company_data, reader_employee_data)
 
+        gross_up_service = GrossUpService(employees)
+        #TODO: gross_up_service.exec()
 
-        #file = request.FILES['file']
-        #decoded_file = file.read().decode('utf-8').splitlines()
-        #reader = csv.DictReader(decoded_file)
-        #for row in reader:
-        #    print(row)
-
-        """
-        Populate with data from the serialization
-        It should be a list of objects to inject to the services layer
-        """
-        # ss_service = SocialSecurityService(100000, 8004084)
-        # wt_service = WithholdingTaxService(True, 0)
-        #
-        # #This service should be used only for excecution of the main process, and the rest of the logic should live within the GrossUpService
-        # gross_up_service = GrossUpService(ss_service, wt_service)
-        #
-        # return_csv = gross_up_service.exec()
-        
-        
-        """data = JSONParser().parse(request)
-        serialized = WithholdingTaxSerializer(data=data)
-        if serialized.is_valid():
-            print('funcional', serialized.data.get('is_foreigner'))
-        else:
-            print('Error in serialization')"""
-    
     return HttpResponse()
 
 @csrf_exempt
