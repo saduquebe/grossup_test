36cb1f6a7e187d3d95f9a38c37d5ef0beee7f724
dangel <bromeroa@unal.edu.co>
2023-09-15 15:45:39 -0500

    fix: social security now commented

--- v1_process/services/social_security_service.py
+++ v1_process/services/social_security_service.py
@@ -4,10 +4,12 @@ Define if these are methods or functions (instance related or not) to define a c
 from v1_process.models import SocialSecurity
 from v1_process.dictionaries.dictionary import *
 
+
 # Calculate total income
 def get_income_calculation(wage_income, unearned_income, paid_vacations):
     total_incomes = wage_income + unearned_income + paid_vacations
     return total_incomes
+
 
 # Calculate base according to Law 1393
 def get_base_law_1393(top_law_1393, unearned_income, paid_vacations, wage_income):
@@ -18,6 +20,7 @@ def get_base_law_1393(top_law_1393, unearned_income, paid_vacations, wage_income
     else:
         excess_law_1393 = unearned_income - cap_40
     return cap_40, excess_law_1393
+
 
 # Calculate Integral Base of Contribution (IBC)
 def get_ibc(wage_income, top_law_1393, unearned_income, paid_vacations,
@@ -43,6 +46,7 @@ def get_ibc(wage_income, top_law_1393, unearned_income, paid_vacations,
         bounded_base_inf = bounded_base_sup
     return base, bounded_base_sup, bounded_base_inf
 
+
 # Get the solidarity percentage from a table of ranges
 # Table of ranges is defined in utils
 def get_solidarity_percentage_table(bounded_base, smlv, ranges):
@@ -51,6 +55,7 @@ def get_solidarity_percentage_table(bounded_base, smlv, ranges):
             return percentage
 
     return 0
+
 
 # Calculate social contributions
 def get_contributions(wage_income, top_law_1393, unearned_income, paid_vacations,
@@ -86,28 +91,9 @@ Service to access namespace
 Fill this service with a list of SocialSecurity objects  
 """
 
+
 # Class to access the social security service
 class SocialSecurityService:
-    '''
-
-    def __init__(self, wage_income, top_law_1393, unearned_income, paid_vacations, ibc_vacations,
-                 integral_percentage_salary, smlv, regime_type, health_contribute, health_percentage,
-                 pension_contribute, pension_percentage, voluntary_mandatory_pension, worked_days, salary_limit):
-        self.wage_income = wage_income
-        self.top_law_1393 = top_law_1393
-        self.unearned_income = unearned_income
-        self.paid_vacations = paid_vacations
-        self.ibc_vacations = ibc_vacations
-        self.integral_percentage_salary = integral_percentage_salary
-        self.smlv = smlv
-        self.regime_type = regime_type
-        self.health_contribute = health_contribute
-        self.health_percentage = health_percentage
-        self.pension_contribute = pension_contribute
-        self.pension_percentage = pension_percentage
-        self.voluntary_mandatory_pension = voluntary_mandatory_pension
-        self.worked_days = worked_days
-        self.salary_limit = salary_limit'''
 
     # Method to execute the service
     def exec(self, social_security: SocialSecurity, ranges):
@@ -133,7 +119,6 @@ class SocialSecurityService:
             SOLIDARITY_CONTRIBUTION: int(fsp),
             VOLUNTARY_MANDATORY_PENSION_CONTRIBUTION: int(mandatory_voluntary_pension)
         }
-        # print(socialSecValues)
         return social_sec_values
 
     def _save_to_database(self):
